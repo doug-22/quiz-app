582dd60dde698d61e86251e62ccd43f9750fc4da
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-29 14:37:00 -0300

    Criando página inicial

---
+++ src/Pages/Main/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import "./style.css";
+
+import Modal from "../../Components/Modal";
+
+const Main = () => {
+
+    const [value, setValue] = useState(1)
+    const handleInputChange = (e) => {
+        setValue(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+
+        console.log(data.number);
+    };
+
+    const [modalVisible, setModalVisible] = useState(false);
+
+    return (
+        <div className="container">
+            <h2>Quantas questões você deseja responder?</h2>
+            <form onSubmit={handleSubmit} className="buttons">
+                <input type="number" name="number" min="1" onChange={handleInputChange} value={value}/>
+                <button type="submit" onClick={() => setModalVisible(true)}>START</button>
+            </form>
+            {modalVisible ? <Modal onClose={() => setModalVisible(false)}/> : null}
+        </div>
+    );
+}
+
+export default Main;
