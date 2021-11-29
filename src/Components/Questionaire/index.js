f3d8ed8071347db0dbdd1f9b1e46f800593504de
Douglas Oliveira <douglas.aguiar410@gmail.com>
2021-11-29 18:03:19 -0300

    Apresentando respostas de forma aleatória

--- src/Components/Questionaire/index.js
+++ src/Components/Questionaire/index.js
@@ -3,20 +3,25 @@ import "./style.css";
 
 import Button from "../Button";
 
-const Questionaire = ({data: {question, correct_answer, incorrect_answers}}) => {
+const Questionaire = ({handleAnswer, data: {question, correct_answer, incorrect_answers}}) => {
+    
+    const shuffledAnswer = [correct_answer, ...incorrect_answers].sort();
+    console.log(shuffledAnswer);
+    
     return(
         <div>
             <div className="content-title">
                 <h2 dangerouslySetInnerHTML={{ __html: question }} />
             </div>
-            <div>
-                <Button onClick={() => handleAnswer(true)} answer={correct_answer} />
-                <Button onClick={() => handleAnswer(false)} answer={incorrect_answers[0]}/>
-                <Button onClick={() => handleAnswer(false)} answer={incorrect_answers[1]}/>
-                <Button onClick={() => handleAnswer(false)} answer={incorrect_answers[2]}/>
+            <div className="content-buttons">
+                <Button className={correct_answer === shuffledAnswer[0] ? "correctAnswer" : ""} onClick={() => handleAnswer(shuffledAnswer[0])} answer={shuffledAnswer[0]} />
+                <Button className={correct_answer === shuffledAnswer[1] ? "correctAnswer" : ""} onClick={() => handleAnswer(shuffledAnswer[1])} answer={shuffledAnswer[1]}/>
+                <Button className={correct_answer === shuffledAnswer[2] ? "correctAnswer" : ""} onClick={() => handleAnswer(shuffledAnswer[2])} answer={shuffledAnswer[2]}/>
+                <Button className={correct_answer === shuffledAnswer[3] ? "correctAnswer" : ""} onClick={() => handleAnswer(shuffledAnswer[3])} answer={shuffledAnswer[3]}/>
             </div>
         </div>
         
     );
+
 }
 export default Questionaire;
